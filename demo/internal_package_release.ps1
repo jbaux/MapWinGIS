8d0d696179a6d8179066508431c486c8823fe79b
jbaux <jbaux@protonmail.com>
2020-08-04 14:49:29 +1000

    The package release script now includes the installer.

--- demo/internal_package_release.ps1
+++ demo/internal_package_release.ps1
@@ -1,13 +1,26 @@
 $result = ".\release.zip"
+# Delete the previous release
 if (Test-Path $result)
 {
     Remove-Item -Path $result -ErrorAction Stop
 }
-Set-Content -Path ".\MWLite.GUI\bin\x86\Release\run_with_logging.bat" -Value "@ECHO OFF`r`nECHO Writing logs to: error.log`r`nMapWindowLite.exe 2> error.log`r`n"
+$MWLitePath = ".\MWLite.GUI\bin\x86\Release"
+# Fetch the latest MapWinGIS
+if (Test-Path "$MWLitePath\MapWinGIS")
+{
+    Remove-Item -Path "$MWLitePath\MapWinGIS" -ErrorAction Stop -Recurse
+}
+Copy-Item ..\src\bin\Win32 -Destination "$MWLitePath\MapWinGIS\" -Recurse
+# Create scripts used in releases
+Set-Content -Path "$MWLitePath\run_with_logging.bat" -Value "@ECHO OFF`r`nECHO Writing logs to: error.log`r`nMapWindowLite.exe 2> error.log`r`n"
+Set-Content -Path "$MWLitePath\install.cmd" -Value "cd MapWinGIS`r`nregsvr32 /u /s MapWinGIS.ocx`r`nregsvr32 MapWinGIS.ocx`r`n"
+# Delete files that aren't needed in releases
+Remove-Item -Path "$MWLitePath\*.pdb", "$MWLitePath\MWLite.Symbology.xml", "$MWLitePath\mwtiles.db3" -ErrorAction Ignore
+# Package up the release
 $compress = @{
-Path= ".\MWLite.GUI\bin\x86\Release\*.dll", ".\MWLite.GUI\bin\x86\Release\*.exe", ".\MWLite.GUI\bin\x86\Release\*.bat"
-CompressionLevel = "Optimal"
-DestinationPath = $result
+    Path = "$MWLitePath\*"
+    CompressionLevel = "Optimal"
+    DestinationPath = $result
 }
 Compress-Archive @compress -ErrorAction Stop
 if (Test-Path $result)
